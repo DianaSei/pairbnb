3f10147df7bde4ff42b714f9d2d1976e5cfc969f
DianaSei <di.hom26@mail.ru>
2019-01-17 09:56:30 +0800

    Fixed minor issues for listing feature and payment flahes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,6 @@ class Listing < ApplicationRecord
 	paginates_per 10
 	belongs_to :user
 	mount_uploaders :images, AvatarUploader
-	belongs_to :booking
 	has_many :bookings
 	has_many :listings_tags
     has_many :tags, through: :listings_tags
